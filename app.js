b006bc184c0da7550911568320dac9dac0e70c7e
Ubaid9513 <shaikhubaid2345@gmail.com>
2024-04-02 03:42:08 +0500

    updated

---
+++ app.js
@@ -0,0 +1,21 @@
+// var date = Date.now();
+// console.log(date);
+
+
+// var date = new Date();
+// console.log(date);
+// console.log(typeof date);
+
+// var type = new Date();
+// console.log(typeof type.toString());
+
+
+// var type = new Date();
+// console.log(type.toString());
+
+// var type = new Date();
+// console.log(type.toString().slice(0,15));
+
+var date = new Date();
+var days = ["SUN","MON","TUE","WED","THUR","FRI","SAT"];
+console.log(days[date.getDay()]);
